2dbac1e009a44050d55febb2058637bd6952268d
snc29654 <snc29654l@nifty.com>
2021-06-27 20:26:04 +0900

    error message discription

--- send.php
+++ send.php
@@ -7,15 +7,15 @@ $param2 = $_POST['param2'];
 //空白チェック
     $errorflag = 0;
 	if ($param0 == null) {
-		$error_param0 = "param0が未入力です";
+		$error_param0 = "param0 is no input";
 		$errorflag = 1;
 	}else{$error_param0 = "";}
 	if ($param1 == null) {
-		$error_param1 = "param1が未入力です";
+		$error_param1 = "param1 is no input";
 		$errorflag = 1;
 	}else{$error_param1 = "";}
 	if ($param2 == null) {
-		$error_param2 = "param2が未入力です";
+		$error_param2 = "param2 is no input";
 		$errorflag = 1;
 	}else{$error_param2 = "";}
 	
