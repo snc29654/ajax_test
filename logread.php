734d90485dc67a0a80e1696affa0a1fa095d2ee3
snc29654 <snc29654l@nifty.com>
2021-06-29 10:05:20 +0900

    書き込みと読み出しを同時に行う

--- logread.php
+++ logread.php
@@ -1,17 +1,8 @@
 <?php
 header("Content-type: text/plain; charset=UTF-8");
-
-//空白チェック
-$errorflag = 0;
-	
-if ($errorflag == 1)
-{
-    
-}
-else
-{
+	//書き込みが早すぎるのでsleepさせる
+	usleep(100);
 	$file = file_get_contents('../php_log.txt');
 	echo $file;
 	   
-}
 ?>
